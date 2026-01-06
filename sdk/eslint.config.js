a451df6caecc4b012a5488c93e6ecd3f6d3cc67e
fanhousanbu <993921@qq.com>
2026-01-06 09:35:25 +0800

    style: condense `@typescript-eslint/no-unused-vars` rule configuration to a single line.

--- sdk/eslint.config.js
+++ sdk/eslint.config.js
@@ -10,10 +10,7 @@ export default tseslint.config(
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_" },
-      ],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
   }
 );
